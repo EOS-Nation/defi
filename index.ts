b67abe47218e6d84d5216c38670a30f280887b35
Denis <carriere.denis@gmail.com>
2020-09-19 15:51:20 -0400

    add coral

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import * as defibox from "./src/defibox";
 import * as yfc from "./src/yfc";
 import * as dfs from "./src/dfs";
 import * as wool from "./src/wool";
+import * as coral from "./src/coral";
 
 export const eos = {
     "dmd": dmd,
@@ -10,4 +11,5 @@ export const eos = {
     "yfc": yfc,
     "dfs": dfs,
     "wool": wool,
+    "coral": coral,
 }
---
+++ src/coral.ts
@@ -0,0 +1,30 @@
+import { JsonRpc } from 'eosjs';
+import { Action, Authorization } from 'eosjs/dist/eosjs-serialize';
+import * as wool from "./wool";
+
+export async function rewards( rpc: JsonRpc, owner: string, authorization: Authorization[] ): Promise<Action[]> {
+    // results
+    const actions = [];
+
+    const contract = "eoscoralpool"
+    for ( const pool_id of await wool.get_pools( rpc, contract ) ) {
+        // must have unclaimed rewards
+        if ( !await wool.is_unclaimed( rpc, owner, contract, pool_id ) ) continue;
+
+        // claim action
+        actions.push({
+            account: contract,
+            name: "claim",
+            authorization,
+            data: {
+                owner,
+                pool_id,
+            }
+        })
+    }
+    return actions;
+}
+
+export async function get_available_claims( rpc: JsonRpc, owner: string, authorization: Authorization[] ): Promise<Action[]> {
+    return [ ...await rewards( rpc, owner, authorization ) ];
+}
