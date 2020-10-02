535719285562e52abb9be8e270fde9b70eee9782
Denis <carriere.denis@gmail.com>
2020-10-01 21:22:24 -0400

    re-order alpha

--- index.ts
+++ index.ts
@@ -1,21 +1,21 @@
-import * as dmd from "./src/dmd";
-import * as defibox from "./src/defibox";
-import * as yfc from "./src/yfc";
-import * as dfs from "./src/dfs";
-import * as wool from "./src/wool";
 import * as coral from "./src/coral";
-import * as superdad from "./src/superdad";
+import * as defibox from "./src/defibox";
+import * as dmd from "./src/dmd";
+import * as dfs from "./src/dfs";
 import * as dolphin from "./src/dolphin";
 import * as rabbit from "./src/rabbit";
+import * as superdad from "./src/superdad";
+import * as wool from "./src/wool";
+import * as yfc from "./src/yfc";
 
 export const eos = {
-    "dmd": dmd,
-    "defibox": defibox,
-    "yfc": yfc,
-    "dfs": dfs,
-    "wool": wool,
     "coral": coral,
-    "superdad": superdad,
+    "defibox": defibox,
+    "dfs": dfs,
+    "dmd": dmd,
     "dolphin": dolphin,
     "rabbit": rabbit,
+    "superdad": superdad,
+    "wool": wool,
+    "yfc": yfc,
 }
