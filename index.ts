d240210e18ab1fa388a31112d4d889b3647a9be5
Denis <carriere.denis@gmail.com>
2020-10-08 21:24:10 -0400

    expose tokens

--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@ import * as rabbit from "./src/rabbit";
 import * as superdad from "./src/superdad";
 import * as wool from "./src/wool";
 import * as yfc from "./src/yfc";
+import * as tokens from "./src/tokens";
 
 export const eos = {
     coral,
@@ -18,4 +19,5 @@ export const eos = {
     superdad,
     wool,
     yfc,
+    tokens
 }
